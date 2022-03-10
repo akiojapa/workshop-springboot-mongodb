ec32866b647db347a5756d8e3da1ee4a6bbc450f
Akio Andrei Marubayashi Matera <akio.matera@unicesumar.edu.br>
2022-03-10 09:00:27 -0300

    First commit
    Começando o ambiente do Projeto

---
+++ src/main/java/com/akioandrei/workshopspringbootmongodb/WorkshopSpringbootMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.akioandrei.workshopspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbApplication.class, args);
+	}
+
+
+}
