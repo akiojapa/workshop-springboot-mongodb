1ee148048fe7ac5c07227845ca24963b75e66454
Akio Andrei Marubayashi Matera <98557376+akiojapa@users.noreply.github.com>
2022-03-10 17:45:38 -0300

    DTO
    Foi implementado o padrão DTO para retornar usuários.

--- src/main/java/com/akioandrei/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/com/akioandrei/workshopspringbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.akioandrei.workshopspringbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.akioandrei.workshopspringbootmongodb.domain.User;
+import com.akioandrei.workshopspringbootmongodb.dto.UserDTO;
 import com.akioandrei.workshopspringbootmongodb.services.UserService;
 
 @RestController
@@ -19,11 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		User akio = new User("1", "Akio Andrei", "akio@gmail.com");
-		User maike = new User("1", "Maike", "maike@gmail.com");
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list =  service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
 
