50360dcd255ca843e7fc6681732dcfc1d34030b0
Akio Andrei Marubayashi Matera <98557376+akiojapa@users.noreply.github.com>
2022-03-10 17:15:55 -0300

    MongoDB connect
    Conectando o programa ao banco de dados MongoDB com repositório e serviços.

---
+++ src/main/java/com/akioandrei/workshopspringbootmongodb/services/UserService.java
@@ -0,0 +1,20 @@
+package com.akioandrei.workshopspringbootmongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.akioandrei.workshopspringbootmongodb.domain.User;
+import com.akioandrei.workshopspringbootmongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();		
+	}
+}
